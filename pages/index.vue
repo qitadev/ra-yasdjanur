8cbf0b48d13816e93f5b39d25a9911f14d894f7e
Albarra Erdanto <albarraerdanto@gmail.com>
2021-11-11 11:33:18 +0700

    create test page for spa test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <Tutorial />
+  <div>
+    <h1>This is home page</h1>
+    <nuxt-link to="/test">
+      Go to test page
+    </nuxt-link>
+  </div>
 </template>
-
-<script>
-export default {}
-</script>
---
+++ pages/test.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>This is test page</h1>
+    <nuxt-link to="/">
+      Go to home
+    </nuxt-link>
+  </div>
+</template>
