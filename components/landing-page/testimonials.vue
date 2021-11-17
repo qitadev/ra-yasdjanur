9d6e57a5ede71c8740be171ce1716f93d3a45f6a
Albarra Naufala <albarraerdanto@gmail.com>
2021-11-17 12:55:05 +0700

    dynamic data testimonial in landing page

--- components/landing-page/testimonials.vue
+++ components/landing-page/testimonials.vue
@@ -6,67 +6,46 @@
     <p class="text-center">
       Pendapat dari alumni dan wali siswa tentang RA Yasdjanur.
     </p>
-    <ul class="mt-8 grid grid-cols-3 gap-4">
-      <li class="bg-white p-4 rounded-lg">
+    <ul class="mt-8 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <li v-for="testimonial in firstThreeTestimonials" :key="testimonial.id" class="bg-white p-4 rounded-lg">
         <div>
           <img
             src="https://res.cloudinary.com/dau2gxgbw/image/upload/v1637115236/ra-yasdjanur/muhammad-ruqi-yaddin-hxLv1jqP0_o-unsplash_1_f0canh.png"
-            alt="Tania Munfisah Guwo"
+            :alt="testimonial.name"
             class="w-14 h-14 inline-block rounded-full"
           >
-          <div class="inline-block align-middle ml-3">
+          <div class="inline-block align-middle p-3">
             <h3 class="font-bold">
-              Tania Munsifah Guwo
+              {{ testimonial.name }}
             </h3>
             <p class="text-sm text-gray-600">
-              Wali Murid Siswa
+              {{ testimonial.designation }}
             </p>
           </div>
         </div>
-        <blockquote class="mt-4">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-        </blockquote>
-      </li>
-      <li class="bg-white p-4 rounded-lg">
-        <div>
-          <img
-            src="https://res.cloudinary.com/dau2gxgbw/image/upload/v1637115236/ra-yasdjanur/muhammad-ruqi-yaddin-hxLv1jqP0_o-unsplash_1_f0canh.png"
-            alt="Tania Munfisah Guwo"
-            class="w-14 h-14 inline-block rounded-full"
-          >
-          <div class="inline-block align-middle ml-3">
-            <h3 class="font-bold">
-              Tania Munsifah Guwo
-            </h3>
-            <p class="text-sm text-gray-600">
-              Wali Murid Siswa
-            </p>
-          </div>
-        </div>
-        <blockquote class="mt-4">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-        </blockquote>
-      </li>
-      <li class="bg-white p-4 rounded-lg">
-        <div>
-          <img
-            src="https://res.cloudinary.com/dau2gxgbw/image/upload/v1637115236/ra-yasdjanur/muhammad-ruqi-yaddin-hxLv1jqP0_o-unsplash_1_f0canh.png"
-            alt="Tania Munfisah Guwo"
-            class="w-14 h-14 inline-block rounded-full"
-          >
-          <div class="inline-block align-middle ml-3">
-            <h3 class="font-bold">
-              Tania Munsifah Guwo
-            </h3>
-            <p class="text-sm text-gray-600">
-              Wali Murid Siswa
-            </p>
-          </div>
-        </div>
-        <blockquote class="mt-4">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        <blockquote class="mt-4 line-clamp-4">
+          {{ testimonial.testimonial }}
         </blockquote>
       </li>
     </ul>
   </section>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      testimonials: []
+    }
+  },
+  async fetch () {
+    const testimonials = await this.$getSheetData(0)
+    this.testimonials = testimonials
+  },
+  computed: {
+    firstThreeTestimonials () {
+      return this.testimonials.slice(0, 3)
+    }
+  }
+}
+</script>
