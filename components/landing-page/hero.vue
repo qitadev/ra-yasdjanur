0666ba445e4baf0358fcefd1a7c9c0e4ced1e64b
Reza Azerly <kiritosao080@gmail.com>
2021-12-22 14:19:39 +0700

    change hero image landing page

--- components/landing-page/hero.vue
+++ components/landing-page/hero.vue
@@ -11,10 +11,11 @@
         Baca selengkapnya
       </nuxt-link>
     </div>
-    <div class="max-w-md mx-auto mt-10 md:mt-0 md:ml-10 relative">
-      <img src="~/assets/img/hero-1.webp" alt="Foto Hero 1" class="w-full -mr-20 z-10 relative">
-      <img src="~/assets/img/dots.webp" alt="Dots" class="absolute bottom-0 -left-6 z-0">
-      <img src="~/assets/img/green-circle.webp" alt="Green Circle" class="absolute w-44 -right-28 top-0 z-0">
+    <div class="max-w-md mx-auto mt-10 md:mt-0 md:ml-10">
+      <img src="~/assets/img/guru-rayasdjanur.webp" alt="Foto Guru RA Yasdjanur" class="w-full">
+<!--      <img src="~/assets/img/hero-1.webp" alt="Foto Hero 1" class="w-full -mr-20 z-10 relative">-->
+<!--      <img src="~/assets/img/dots.webp" alt="Dots" class="absolute bottom-0 -left-6 z-0">-->
+<!--      <img src="~/assets/img/green-circle.webp" alt="Green Circle" class="absolute w-44 -right-28 top-0 z-0">-->
     </div>
   </section>
 </template>
